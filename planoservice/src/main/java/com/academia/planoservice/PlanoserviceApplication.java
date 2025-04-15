881401d408b90865a7c4000b86f214183239ac40
pedro <pedrohecan@gmail.com>
2025-04-14 21:12:29 -0300

    Micro serviço de plano criado

---
+++ planoservice/src/main/java/com/academia/planoservice/PlanoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.academia.planoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoserviceApplication.class, args);
+	}
+
+}
---
+++ planoservice/src/test/java/com/academia/planoservice/PlanoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.planoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
